88dbf2be35fd49657b0899caf2d0ae316645f9ec
Gabriel Costa Toledo <GLTD@gft.com>
2025-12-08 15:02:18 -0300

    initial setup

---
+++ procedures/src/test/java/com/starter/procedures/ProceduresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.procedures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProceduresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
