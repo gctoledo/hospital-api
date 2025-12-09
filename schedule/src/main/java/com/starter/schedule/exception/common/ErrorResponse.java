d7a1db8a1b79abeca63a1074e26cad23c9e14f2a
Gabriel Costa Toledo <GLTD@gft.com>
2025-12-09 12:35:36 -0300

    feat(schedule): add exception handling

---
+++ schedule/src/main/java/com/starter/schedule/exception/common/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.starter.schedule.exception.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+}
+
